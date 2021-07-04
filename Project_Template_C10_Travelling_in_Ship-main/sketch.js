110dae9f32facb968a7e8a9c7cc4ac0ed815ee21
Abhiram-123 <86110366+Abhiram-123@users.noreply.github.com>
2021-07-04 20:14:16 +0530

    Add files via upload

---
+++ Project_Template_C10_Travelling_in_Ship-main/sketch.js
@@ -0,0 +1,26 @@
+var sea
+var ship
+
+function preload(){
+ seaImg=loadImage("sea.png");
+ shipImg=loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  sea=createSprite(200,200,0,0);
+  sea.addImage(seaImg);
+  sea.velocityX=-0.8;
+  sea.scale=0.3;
+  ship=createSprite(50,250,10,10);
+  ship.addAnimation("ship4",shipImg);
+  ship.scale=0.1;
+  if(sea.X<0){
+    sea.X = sea.width/2;
+  }
+}
+
+function draw() {
+ drawSprites()
+  
+}
